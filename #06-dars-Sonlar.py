bc55979eaf41d3ee281a55bab974f22b082bb878
Eldor Baxtiyorov <91687326+eldorbaxtiyorov@users.noreply.github.com>
2021-09-30 22:14:21 +0500

    Add files via upload

---
+++ #06-dars-Sonlar.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 17:07:05 2021
+
+6-dars Sonlar
+
+@author: Eldor
+#1-mashq
+son=int(input("Istalgan son kiriting:\n>>>"))
+print(son,"ning kvadrati:",son**2,"\n",son," ning kubi:",son**3)
+#2-mashq
+yosh=int(input("Yoshingiz nechida?\n>>>"))
+t_yil=2021-yosh
+print("Siz",t_yil,"tug'ilgansiz")
+
+#3-mashq
+
+son1=float(input("Birichi sonni kiriting:"))
+son2=float(input("Ikkinchi sonni kiriting:"))
+
+print(f"{son1}+{son2}=", son1+son2,'\n',f"{son1}-{son2}=", son1-son2,'\n',f"{son1}*{son2}=", son1*son2,'\n',\
+      f"{son1}/{son2}=", son1/son2)
+    """
